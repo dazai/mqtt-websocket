dcc1f2795c3a78b415f279f73616e3e450830537
dazai <samihsboui@gmail.com>
2021-06-07 18:23:14 +0200

    changed cros origin

--- src/main/java/com/example/iot/configuration/SecurityConfigurer.java
+++ src/main/java/com/example/iot/configuration/SecurityConfigurer.java
@@ -59,7 +59,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NotNull CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://138.197.130.191");
+                registry.addMapping("/**").allowedOrigins("https://www.watertec.tecsar.net");
             }
         };
     }
